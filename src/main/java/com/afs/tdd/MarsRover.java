1a637f76ce23b28973840bfb43913538140f5002
Calvin Chu <calvin.chu@oocl.com>
2020-11-26 17:05:58 +0800

    [mars-rover][Calvin] feat: implemented case for 00N, M -> 01N

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -3,10 +3,17 @@ package com.afs.tdd;
 import java.util.List;
 
 public class MarsRover {
+    private NavigationMeta navigationMeta;
     public MarsRover(int locationX, int locationY, String direction) {
+        this.navigationMeta = new NavigationMeta(locationX, locationY, direction);
     }
 
-    public NavigationMeta executeCommands(List<String> m) {
-        return null;
+    public NavigationMeta executeCommands(List<String> commands) {
+        commands.forEach(this::executeCommand);
+        return this.navigationMeta;
+    }
+
+    private void executeCommand(String command) {
+        navigationMeta.moveOneSpace();
     }
 }
--- src/main/java/com/afs/tdd/NavigationMeta.java
+++ src/main/java/com/afs/tdd/NavigationMeta.java
@@ -1,15 +1,29 @@
 package com.afs.tdd;
 
 public class NavigationMeta {
+    int locationX;
+    int locationY;
+    String direction;
+
+    public NavigationMeta(int locationX, int locationY, String direction) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
     public int getX() {
         return 0;
     }
 
     public int getY() {
-        return 0;
+        return this.locationY;
     }
 
     public String getDirection() {
-        return null;
+        return this.direction;
+    }
+
+    public void moveOneSpace() {
+        ++locationY;
     }
 }
